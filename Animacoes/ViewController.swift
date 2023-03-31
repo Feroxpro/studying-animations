63cc3844f513afd79cd4ad05b6fdc91bdb03d22c
Felipe Domingos <felipedomingos@MacBook-Pro-de-Felipe.local>
2023-03-30 23:56:08 -0300

    Estudo inicial de animacoes

--- Animacoes/ViewController.swift
+++ Animacoes/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var animacaoView: UIView!
+    
 
+    //MARK: - View life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    //MARK: - IBAction
+    
+    @IBAction func iniciaAnimacao(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveLinear], animations: {
+            self.animacaoView.frame = CGRect(
+                x: self.animacaoView.frame.origin.x,
+                y: 680,
+                width: self.animacaoView.frame.size.width,
+                height: self.animacaoView.frame.size.height)
+        }, completion: nil)
+    }
 }
 
